c5b7fe2ca9284dcd46992bf18a4cbba5d904ddce
Kaiter Wu <wukaiter@gmail.com>
2023-02-09 15:33:39 -0500

    finishing associations

--- practice-for-ch-sql-associations-long-practice-main/app/models/course.rb
+++ practice-for-ch-sql-associations-long-practice-main/app/models/course.rb
@@ -10,4 +10,23 @@
 #  updated_at    :datetime         not null
 #
 class Course < ApplicationRecord
+    has_many(
+        :enrollments,
+        class_name: "Enrollment",
+        foreign_key: :course_id,
+        primary_key: :id
+    )
+
+   belongs_to(
+        :prerequisites,
+        class_name: "Course",
+        foreign_key: :prereq_id,
+        primary_key: :id 
+    )
+
+    
+ 
+    has_many :enrolled_students, through: :enrollments, source: :student
+    # has_one :prerequisite, through: :enrollments, source: :course
+
 end
