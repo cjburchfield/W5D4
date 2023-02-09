d4af651c975ca23304c0a539284080f720c8d787
Jamie Burchfield <burchfield.camilla@gmail.com>
2023-02-09 15:05:58 -0500

    Finished User

--- practice-for-ch-sql-associations-long-practice-main/app/models/user.rb
+++ practice-for-ch-sql-associations-long-practice-main/app/models/user.rb
@@ -8,5 +8,20 @@
 #  updated_at :datetime         not null
 #
 class User < ApplicationRecord
+    has_many(
+        :enrollments,
+        class_name: "Enrollment",
+        foreign_key: :student_id,
+        primary_key: :id
+    )
+
+    # # has_many (
+    # #     :enrolled_courses,
+    # #     class_name: "Course",
+    # #     foreign_key: user_id,
+    # #     primary_key: id
+    # # )
+
+    has_many :enrolled_courses, through: :enrollments, source: :course
 
 end
